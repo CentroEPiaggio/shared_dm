71c66144eb1984419bac628e9ac5b038f3cc88c4
MirkoFerrati <mirko.ferrati@gmail.com>
2014-12-17 01:58:44 +0100

    Added some ids to Grasp and Object

--- include/dual_manipulation_shared/grasp.h
+++ include/dual_manipulation_shared/grasp.h
@@ -1,8 +1,11 @@
 #ifndef SHARED_GRASP_H
 #define SHARED_GRASP_H
+#include <string>
 
 class Grasp
 {
+public:
+    std::string name;
 };
 
 
--- include/dual_manipulation_shared/object.h
+++ include/dual_manipulation_shared/object.h
@@ -1,8 +1,11 @@
 #ifndef DUAL_MANIPULATION_SHARED_OBJECT
 #define DUAL_MANIPULATION_SHARED_OBJECT
+#include <string>
 
 class Object
 {
+public:
+    std::string id;
 };
 
 #endif
