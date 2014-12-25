7b053c0d69458dce3f37b2e984dc0877e2be4e01
MirkoFerrati <mirko.ferrati@gmail.com>
2014-12-26 00:53:46 +0100

    added a simple stream utils to provide std::cout<<std::map/vector/set/pair/list without having to iterate in the code

---
+++ include/dual_manipulation_shared/stream_utils.h
@@ -0,0 +1,61 @@
+#ifndef STREAM_UTILS_H
+#define STREAM_UTILS_H
+
+
+#include <map>
+#include <string>
+#include <vector>
+#include <list>
+#include <iostream>
+#include <fstream>
+#include <forward_list>
+
+template< typename K,typename T>//,typename C, typename Alloc >
+std::ostream& operator<<( std::ostream& os, const std::map<K,T>& m )
+{
+    for (typename std::map<K,T>::const_iterator it=m.begin();it!=m.end();++it)
+        os<<it->first <<" "<<it->second <<" ";
+    os<<std::endl;
+    return os;
+}
+
+template< typename K,typename T,typename C >
+std::ostream& operator<<( std::ostream& os, const std::tuple<K,T,C>& m )
+{
+    os<<std::get<0>(m)<<" "<<std::get<1>(m)<<" "<<std::get<2>(m);
+    os<<std::endl;
+    return os;
+}
+
+template< typename T>//,typename C, typename Alloc >
+std::ostream& operator<<( std::ostream& os, const std::vector<T>& m )
+{
+    for (typename std::vector<T>::const_iterator it=m.begin();it!=m.end();++it)
+        os<<(*it)<<" ";
+    return os;
+}
+
+
+template< typename T>//,typename C, typename Alloc >
+std::ostream& operator<<( std::ostream& os, const std::list<T>& m )
+{
+    for (typename std::list<T>::const_iterator it=m.begin();it!=m.end();++it)
+        os<<(*it)<<" ";
+    return os;
+}
+
+template< typename T>//,typename C, typename Alloc >
+std::ostream& operator<<( std::ostream& os, const std::set<T>& m )
+{
+    for (typename std::set<T>::const_iterator it=m.begin();it!=m.end();++it)
+        os<<(*it)<<" ";
+    return os;
+}
+
+template<typename T, typename K>
+std::ostream& operator<<(std::ostream& os, const std::pair<T,K>& m)
+{
+    os<<m.first<<" "<<m.second<<" "<<std::endl;
+    return os;
+}
+#endif
