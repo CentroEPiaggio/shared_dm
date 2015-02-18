b28139afe8000d1b56210b832e33779d566ff502
hamalMarino <hamal.marino@gmail.com>
2015-02-18 14:20:09 +0100

    test_serialization and test_deserialization used to check for a simple grasp DB

--- test/test_deserialization.cpp
+++ test/test_deserialization.cpp
@@ -4,6 +4,31 @@
 
 #include "dual_manipulation_shared/serialization_utils.h"
 
+#include <kdl/frames.hpp>
+#include <kdl_conversions/kdl_msg.h>
+
+#include <visualization_msgs/Marker.h>
+
+void publish_marker_utility(ros::Publisher& vis_pub, geometry_msgs::Pose& pose, double scale=0.05)
+{
+  visualization_msgs::Marker marker;
+  marker.header.frame_id = "world";
+  marker.header.stamp = ros::Time();
+  marker.ns = "my_namespace";
+  marker.id = 0;
+  marker.type = visualization_msgs::Marker::CUBE;
+  marker.action = visualization_msgs::Marker::ADD;
+  marker.pose = pose;
+  marker.scale.x = scale/2.0;
+  marker.scale.y = scale;
+  marker.scale.z = scale*2;
+  marker.color.a = 1.0;
+  marker.color.r = 1.0;
+  marker.color.g = 0.0;
+  marker.color.b = 0.0;
+  vis_pub.publish( marker );
+}
+
 int main(int argc, char **argv)
 {
     std::cout<<std::endl;
@@ -14,41 +39,79 @@ int main(int argc, char **argv)
 
     ros::NodeHandle n;
     ros::ServiceClient client_obj = n.serviceClient<dual_manipulation_shared::scene_object_service>("scene_object_ros_service");
+    ros::Publisher vis_pub = n.advertise<visualization_msgs::Marker>( "visualization_marker", 0 );
     
     dual_manipulation_shared::ik_service srv;
     dual_manipulation_shared::scene_object_service srv_obj;
+    
+    std::vector<std::string> grasp_links;
+    std::vector<geometry_msgs::Pose> grasp_poses;
     
     srv_obj.request.command = "add";
     
     bool ok = true;
     int i = 0;
     
+    int object_id = 1;
+    
     while (ok)
     {
       
-      ok = deserialize_ik(srv.request,"grasp" + std::to_string(i++));
+      ok = deserialize_ik(srv.request,"object" + std::to_string(object_id) + "/grasp" + std::to_string(i++));
       
       if (ok)
-	std::cout << "Deserialization " << i-1 << " OK!" << std::endl;
+      {
+	std::cout << "Deserialization object" + std::to_string(object_id) << "/grasp" << i-1 << " OK!" << std::endl;
+	
+	srv_obj.request.attObject = srv.request.attObject;
+	
+	if (client_obj.call(srv_obj))
+	{
+	    ROS_INFO("IK_control:test_grasping : %s object %s request accepted: %d", srv_obj.request.command.c_str(),srv_obj.request.attObject.object.id.c_str(), (int)srv_obj.response.ack);
+	}
+	else
+	{
+	    ROS_ERROR("IK_control:test_grasping : Failed to call service dual_manipulation_shared::scene_object_service: %s %s",srv_obj.request.command.c_str(),srv_obj.request.attObject.object.id.c_str());
+	}
+	
+	grasp_links.push_back(srv.request.attObject.object.header.frame_id);
+	grasp_poses.push_back(srv.request.ee_pose.back());
+	
+	// usleep(200000);
+      }
       else
       {
-	std::cout << "Stopped at deserialization " << i-1 << " OK!" << std::endl;
-	return -1;
+	std::cout << "Stopped at deserialization object" + std::to_string(object_id) << "/grasp" << i-1 << std::endl;
       }
+    }
+    
+    srv_obj.request.attObject.object.mesh_poses.clear();
+    geometry_msgs::Pose neutral,marker_pose;
+    neutral.position.x = -0.5;
+    neutral.position.y = 0.0;
+    neutral.position.z = 0.5;
+    neutral.orientation.w = 1.0;
+    srv_obj.request.attObject.object.mesh_poses.push_back(neutral);
+    
+    client_obj.call(srv_obj);
+    
+    KDL::Frame cyl_kdl;
+    tf::poseMsgToKDL(neutral,cyl_kdl);
+    
+    for (int i=0; i<grasp_poses.size(); ++i)
+    {
+      KDL::Frame fi;
+      tf::poseMsgToKDL(grasp_poses.at(i),fi);
       
-      srv_obj.request.attObject = srv.request.attObject;
+      tf::poseKDLToMsg(cyl_kdl*(fi.Inverse()),marker_pose);
       
-      if (client_obj.call(srv_obj))
-      {
-	  ROS_INFO("IK_control:test_grasping : %s object %s request accepted: %d", srv_obj.request.command.c_str(),srv_obj.request.attObject.object.id.c_str(), (int)srv_obj.response.ack);
-      }
+      publish_marker_utility(vis_pub,marker_pose);
+      
+      if (grasp_links.at(i) != "world")
+	usleep(200000);
       else
-      {
-	  ROS_ERROR("IK_control:test_grasping : Failed to call service dual_manipulation_shared::scene_object_service: %s %s",srv_obj.request.command.c_str(),srv_obj.request.attObject.object.id.c_str());
-      }
-      
-      usleep(200000);
-      
+	usleep(200000);
+	// sleep(2);
     }
     
     ros::spinOnce();
--- test/test_serialization.cpp
+++ test/test_serialization.cpp
@@ -10,13 +10,13 @@
 // definitions used to generate grasps for a cylinder
 #define H 0.2
 #define R 0.05
-#define mid_palm_height 0.05
+#define mid_palm_height 0.0 //0.05
 #define grasp_distance 0.05
 
 std::vector<std::string> ee_vector{"left_hand","right_hand","table"};
-std::vector<double> x_rot{-M_PI/2.0,M_PI/2.0};
+std::vector<double> x_rot{/*-M_PI/2.0,*/M_PI/2.0};
 std::vector<double> z_transl{-H/4.0,0.0,H/4.0};
-std::vector<double> z_rot{-M_PI/2.0,0.0,M_PI/2.0,M_PI};
+std::vector<double> z_rot{-M_PI/2.0,/*0.0,*/M_PI/2.0/*,M_PI*/};
 std::vector<double> y_rot{-M_PI/2.0,M_PI/2.0};
 
 int main(int argc, char **argv)
@@ -49,8 +49,6 @@ int main(int argc, char **argv)
     common_frame.p.data[1] = 0.0;
     common_frame.p.data[2] = 0.0;
     
-    common_frame.p.data[2] = mid_palm_height;
-
     attached_object.object.id = "Cylinder";
     // this will be interpreted as the object ID (to read in the DB)
     attached_object.weight = 1.0;
@@ -77,21 +75,37 @@ int main(int argc, char **argv)
       
       //side grasps
       if(ee == "left_hand" || ee == "right_hand")
+      {
 	common_frame.p.data[0] = R + grasp_distance;
+	common_frame.p.data[2] = mid_palm_height;
+      }
       else
+      {
 	common_frame.p.data[0] = R;
+	common_frame.p.data[2] = 0.0;
+      }
 
       for (auto x_rot_i:x_rot)
       {
 	for (auto z_transl_i:z_transl)
 	{
+	  // if hands, do not consider mid-height
+	  if ((ee == "left_hand" || ee == "right_hand") && (z_transl_i==0.0))
+	    continue;
 	  // if ee table: do not translate
-	  if (!(ee == "left_hand" || ee == "right_hand") && (z_transl_i!=0.0))
+	  else if (!(ee == "left_hand" || ee == "right_hand") && (z_transl_i!=0.0))
 	    continue;
 	    
 	  for (auto z_rot_i:z_rot)
 	  {
 	    obj_frame = common_frame;
+	    
+	    // for the table, align x with z (rotation around "fixed" y of -pi/2)
+	    if(!(ee == "left_hand" || ee == "right_hand"))
+	    {
+	      obj_frame = KDL::Frame( KDL::Rotation::RotY(-M_PI/2.0) )*obj_frame;
+	    }
+	    
 	    obj_frame.M.DoRotX(x_rot_i);
 	    obj_frame.p += obj_frame.M*KDL::Vector(0,0,z_transl_i);
 	    obj_frame.M.DoRotZ(z_rot_i);
@@ -106,27 +120,45 @@ int main(int argc, char **argv)
 	    srv.request.ee_pose.push_back(obj_pose);
 	    
 	    // save the obtained grasp
-	    if(serialize_ik(srv.request,"grasp" + std::to_string(counter++)))
-	      std::cout << "Serialization " << counter-1 << " OK!" << std::endl;
+	  if(serialize_ik(srv.request,"object" + std::to_string((int)attached_object.weight) + "/grasp" + std::to_string(counter++)))
+	    std::cout << "Serialization object" + std::to_string((int)attached_object.weight) << "/grasp" << counter-1 << " OK!" << std::endl;
+// 	  if(serialize_ik(srv.request,"grasp" + std::to_string(counter++)))
+// 	      std::cout << "Serialization " << counter-1 << " OK!" << std::endl;
 	    else
+	    {
+	      std::cout << "Error in serialization object" + std::to_string((int)attached_object.weight) << "/grasp" << counter-1 << "!" << std::endl;
 	      return -1;
+	    }
 	  }
 	}
       }
       
       //top/bottom grasps
       if(ee == "left_hand" || ee == "right_hand")
+      {
 	common_frame.p.data[0] = H/2.0 + grasp_distance;
+	common_frame.p.data[2] = mid_palm_height;
+      }
       else
-	common_frame.p.data[0] = 0.0;
+      {
+	common_frame.p.data[0] = H/2.0;
+	common_frame.p.data[2] = 0.0;
+      }
 
       for (auto y_rot_i:y_rot)
       {
-	for (auto z_rot_i:z_rot)
+// 	for (auto z_rot_i:z_rot)
 	{
 	  obj_frame = common_frame;
+	  
+	  // for the table, align x with z (rotation around "fixed" y of -pi/2)
+	  if(!(ee == "left_hand" || ee == "right_hand"))
+	  {
+	    obj_frame = KDL::Frame( KDL::Rotation::RotY(-M_PI/2.0) )*obj_frame;
+	  }
+	
 	  obj_frame.M.DoRotY(y_rot_i);
-	  obj_frame.M.DoRotZ(z_rot_i);
+// 	  obj_frame.M.DoRotZ(z_rot_i);
 	  
 	  // convert from KDL to geometry_msgs
 	  tf::poseKDLToMsg(obj_frame,obj_pose);
@@ -138,10 +170,13 @@ int main(int argc, char **argv)
 	  srv.request.ee_pose.push_back(obj_pose);
 	  
 	  // save the obtained grasp
-	  if(serialize_ik(srv.request,"grasp" + std::to_string(counter++)))
-	    std::cout << "Serialization " << counter-1 << " OK!" << std::endl;
+	  if(serialize_ik(srv.request,"object" + std::to_string((int)attached_object.weight) + "/grasp" + std::to_string(counter++)))
+	    std::cout << "Serialization object" + std::to_string((int)attached_object.weight) << "/grasp" << counter-1 << " OK!" << std::endl;
 	  else
+	  {
+	    std::cout << "Error in serialization object" + std::to_string((int)attached_object.weight) << "/grasp" << counter-1 << "!" << std::endl;
 	    return -1;
+	  }
 	}
       }
       
