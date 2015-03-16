6f34e6556a711b10053b11c9113cd8bce4c5ac8e
hamalMarino <hamal.marino@gmail.com>
2015-03-16 16:56:27 +0100

    Added parsing_utility in shared

---
+++ include/dual_manipulation_shared/parsing_utils.h
@@ -0,0 +1,14 @@
+#ifndef PARSING_UTILS_H
+#define PARSING_UTILS_H
+
+#include <XmlRpcValue.h>
+
+void parseSingleParameter(XmlRpc::XmlRpcValue& params, double& param, std::string param_name);
+
+void parseSingleParameter(XmlRpc::XmlRpcValue& params, std::vector<double>& param, std::string param_name);
+
+void parseSingleParameter(XmlRpc::XmlRpcValue& params, std::vector<std::string>& param, std::string param_name);
+
+void parseSingleParameter(XmlRpc::XmlRpcValue& params, std::map< std::string, std::string >& param, std::string param_name, std::vector< std::string > names_list);
+
+#endif //PARSING_UTILS_H
---
+++ src/parsing_utils.cpp
@@ -0,0 +1,107 @@
+#include "../include/dual_manipulation_shared/parsing_utils.h"
+
+// only used for INFO, ASSERT, and WARN functions
+#include "ros/ros.h"
+
+void parseSingleParameter(XmlRpc::XmlRpcValue& params, double& param, std::string param_name)
+{
+    if( !params.hasMember(param_name) )
+    {
+        ROS_WARN_STREAM("No value for " << param_name <<". Check the yaml configuration, we will use " << param << " as default value.");
+	return;
+    }
+
+    ROS_ASSERT(params[param_name].getType() == XmlRpc::XmlRpcValue::TypeDouble);
+    param = (double) params[param_name];
+    ROS_INFO_STREAM("Read parameter " << param_name << " = " << param);
+
+    return;
+}
+
+void parseSingleParameter(XmlRpc::XmlRpcValue& params, std::vector< double >& param, std::string param_name)
+{
+    std::string vector_str;
+    
+    if( !params.hasMember(param_name) )
+    {
+	for(auto item:param)
+	  vector_str.append( std::to_string(item) ).append(" | ");
+	
+        ROS_WARN_STREAM("No value for " << param_name <<". Check the yaml configuration, we will use | " << vector_str << "as default value.");
+	return;
+    }
+
+    ROS_ASSERT(params[param_name].getType() == XmlRpc::XmlRpcValue::TypeArray);
+    
+    param.clear();
+    for(int i=0; i< params[param_name].size(); i++)
+      param.push_back( (double) params[param_name][i]);
+    
+    for(auto item:param)
+      vector_str.append( std::to_string(item) ).append(" | ");
+    
+    ROS_INFO_STREAM("Read parameter " << param_name << " = | " << vector_str);
+
+    return;
+}
+
+void parseSingleParameter(XmlRpc::XmlRpcValue& params, std::vector< std::string >& param, std::string param_name)
+{
+    std::string vector_str;
+    
+    if( !params.hasMember(param_name) )
+    {
+	for(auto item:param)
+	  vector_str.append( item ).append(" | ");
+	
+        ROS_WARN_STREAM("No value for " << param_name <<". Check the yaml configuration, we will use | " << vector_str << "as default value.");
+	return;
+    }
+
+    ROS_ASSERT(params[param_name].getType() == XmlRpc::XmlRpcValue::TypeArray);
+    
+    param.clear();
+    for(int i=0; i< params[param_name].size(); i++)
+      param.push_back( (std::string) params[param_name][i]);
+    
+    for(auto item:param)
+      vector_str.append( item ).append(" | ");
+    
+    ROS_INFO_STREAM("Read parameter " << param_name << " = | " << vector_str);
+
+    return;
+}
+
+void parseSingleParameter(XmlRpc::XmlRpcValue& params, std::map< std::string, std::string >& param, std::string param_name, std::vector< std::string > names_list)
+{
+    std::string vector_str;
+    
+    if( !params.hasMember(param_name) )
+    {
+	for(auto item:param)
+	  vector_str.append( item.first ).append(" : ").append( item.second ).append(" | ");
+	
+        ROS_WARN_STREAM("No value for " << param_name <<". Check the yaml configuration, we will use | " << vector_str << "as default value.");
+	return;
+    }
+
+    ROS_ASSERT(params[param_name].getType() == XmlRpc::XmlRpcValue::TypeStruct);
+    
+    for(int i=0; i< names_list.size(); i++)
+    {
+      if( !params[param_name].hasMember(names_list.at(i)) )
+      {
+	ROS_WARN_STREAM("No value for " << param_name << ":" << names_list.at(i) << ". Check the yaml configuration.");
+	continue;
+      }
+      param[names_list.at(i)] = (std::string)params[param_name][names_list.at(i)];
+    }
+    
+    for(auto item:param)
+      vector_str.append( item.first ).append(" : ").append( item.second ).append(" | ");
+    
+    ROS_INFO_STREAM("Read parameter " << param_name << " = | " << vector_str);
+
+    return;
+}
+
