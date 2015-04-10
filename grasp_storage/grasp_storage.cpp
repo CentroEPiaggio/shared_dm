19a6343e575fa74005db32ddc698eaadc4033f6f
hamalMarino <hamal.marino@gmail.com>
2015-04-10 20:30:54 +0200

    completely refactored grasp_storage, now much cleaner (could also load data from external files)

--- grasp_storage/grasp_storage.cpp
+++ grasp_storage/grasp_storage.cpp
@@ -1,284 +1,340 @@
 #include "grasp_storage.h"
 #include <sqlite3.h>
-#include "dual_manipulation_shared/ik_service.h"
+#include "dual_manipulation_shared/grasp_trajectory.h"
 #include "dual_manipulation_shared/serialization_utils.h"
 
+#define ABSURD_TIME 100.0
+#define THREAD_TIME 10000
+
 std::string str_quotesql( const std::string& s ) {
-    return std::string("'") + s + std::string("'");
+  return std::string("'") + s + std::string("'");
 }
 
 std::string int_quotesql( const int& s ) {
-    return std::string("'") + std::to_string(s) + std::string("'");
+  return std::string("'") + std::to_string(s) + std::string("'");
 }
 
-bool grasp_storage::get_transform(tf::StampedTransform& result,std::string target, std::string source)
+bool grasp_storage::get_transform(tf::StampedTransform& result, std::string target, std::string source, double wait_time)
 {
-    std::string err_msg;
-
-    tf::StampedTransform target_T_source;
-
-    if(!tf.waitForTransform(target,source,ros::Time(0), ros::Duration(1.0), ros::Duration(0.01), &err_msg))
-    {
-	ROS_ERROR("Error in tf: %s",err_msg.c_str());
-	target_T_source.setIdentity();
-	return false;
-    }
-    else tf.lookupTransform(target,source, ros::Time(0), target_T_source);
-    result=target_T_source;
-    return true;
+  std::string err_msg;
+  
+  tf::StampedTransform target_T_source;
+  
+  if(!tf.waitForTransform(target,source,ros::Time(0), ros::Duration(wait_time), ros::Duration(0.01), &err_msg))
+  {
+    ROS_ERROR("Error in tf: %s",err_msg.c_str());
+    target_T_source.setIdentity();
+    return false;
+  }
+  else
+    tf.lookupTransform(target,source, ros::Time(0), target_T_source);
+  
+  result=target_T_source;
+  
+  return true;
 }
 
-grasp_storage::grasp_storage(int object_id_, int end_effector_id_, std::string grasp_name_):object_id(object_id_),end_effector_id(end_effector_id_), grasp_name(grasp_name_)
+grasp_storage::grasp_storage(std::string db_name)
 {
-    path_to_db = ros::package::getPath("dual_manipulation_grasp_db");
-    
-    object_name_map[1]="FakeCylinder";
-    object_name_map[2]="Pot";
-    object_name_map[3]="Cylinder";
-    
-    ee_name_map[1]="left_hand";
-    ee_name_map[2]="right_hand";
-    ee_name_map[3]="table";
-    
-    world_tf = "/world";
-    hand_tf ="/right_hand_palm_link";
-    object_tf = "/cylinder";
-
-// //   SIDE 180  
-//     snapshots.push_back(ros::Time( 1425502044.020805 ));
-//     snapshots.push_back(ros::Time( 1425502048.240805 ));
-//     snapshots.push_back(ros::Time( 1425502054.100805 ));
-    
-    //   SIDE 180 new
-    snapshots.push_back(ros::Time( 1425502042.224888 ));
-    snapshots.push_back(ros::Time( 1425502048.240805 ));
-    snapshots.push_back(ros::Time( 1425502054.100805 ));
-//   SIDE 0  
-//     snapshots.push_back(ros::Time( 1425502255.675265 ));
-//     snapshots.push_back(ros::Time( 1425502256.155265 ));
-//     snapshots.push_back(ros::Time( 1425502258.665265 ));    
-    
-//   TOP 0  
-//     snapshots.push_back(ros::Time( 1425501243.205473 ));
-//     snapshots.push_back(ros::Time( 1425501245.147348 ));
-//     snapshots.push_back(ros::Time( 1425501249.697563 ));
-
-//   TOP 90  
-//     snapshots.push_back(ros::Time( 1425501625.778888 ));
-//     snapshots.push_back(ros::Time( 1425501627.254295 ));
-//     snapshots.push_back(ros::Time( 1425501631.434716 ));
+  db_mapper_ = boost::shared_ptr<databaseMapper>(new databaseMapper(db_name));
+  db_writer_ = boost::shared_ptr<databaseWriter>(new databaseWriter(db_name));
 }
 
-void grasp_storage::insert_db_entry()
+void grasp_storage::reset_grasp_storage()
 {
-    sqlite3 *db;
-    sqlite3_stmt * stmt;
+  obj0_trajectory.clear();
+  hand_objF = geometry_msgs::Pose();
+  world_T_obj0 = tf::StampedTransform();
+  
+  grasp_info_set = false;
+}
 
-    std::string empty="";
+bool grasp_storage::set_grasp_info(int object_id_, int end_effector_id_, std::string grasp_name_, std::vector< ros::Time > snapshots_, std::string world_tf_, std::string hand_tf_, std::string object_tf_)
+{
+  if(!stop_thread)
+  {
+    ROS_WARN_STREAM("grasp_storage::set_grasp_info : cannot set grasp_info while thread is running!");
+    return false;
+  }
+  
+  if(db_mapper_->Objects.count(object_id_) == 0)
+  {
+    ROS_ERROR_STREAM("grasp_storage::set_grasp_info : No object present in the DB with ID " << object_id_);
+    return false;
+  }
+  
+  if(db_mapper_->EndEffectors.count(end_effector_id_) == 0)
+  {
+    ROS_ERROR_STREAM("grasp_storage::set_grasp_info : No end-effector present in the DB with ID " << end_effector_id_);
+    return false;
+  }
+  
+  if(snapshots_.empty())
+  {
+    ROS_ERROR_STREAM("grasp_storage::set_grasp_info : snapshots vector is empty!");
+    return false;
+  }
+  
+  object_id = object_id_;
+  end_effector_id = end_effector_id_;
+  grasp_name = grasp_name_;
+  
+  world_tf = world_tf_;
+  hand_tf = hand_tf_;
+  object_tf = object_tf_;
+  
+  snapshots.clear();
+  snapshots = snapshots_;
+  
+  grasp_info_set = true;
+  
+  return true;
+}
 
-    //INSERT INTO Grasps (Object_id, EndEffector_id, Grasp_id, Grasp_info, Grasp_name) VALUES ('3','1','','gatto')
+bool grasp_storage::record_grasp()
+{
+  ROS_INFO_STREAM("grasp_storage::record_grasp : start recording trajectory");
+  
+  if(!grasp_info_set)
+  {
+    ROS_WARN_STREAM("grasp_storage::record_grasp : grasp_info not set yet, call set_grasp_info() first!!!");
+    return false;
+  }
+  
+  if(stop_thread)
+  {
+    usleep(2*THREAD_TIME);
+    save_start = true;
+    stop_thread = false;
+    tf_periodic_listener = boost::shared_ptr<std::thread>(new std::thread(&grasp_storage::thread_body, this));
+  }
+  else
+    ROS_WARN_STREAM("grasp_storage::record_grasp : thread was already running - not starting again");
+  
+  return true;
+}
 
-    std::string sqlstatement =
-    "INSERT INTO Grasps (Object_id, EndEffector_id, Grasp_info, Grasp_name) VALUES ("
-    + int_quotesql(object_id) + ","
-    + int_quotesql(end_effector_id) + ","
-    + str_quotesql(empty) + ","
-    + str_quotesql(grasp_name) + ");";
+bool grasp_storage::save_recording()
+{
+  bool ok = true;
+  if(!grasp_info_set)
+  {
+    ROS_ERROR_STREAM("grasp_storage::save_recording : grasp_info not set yet, call set_grasp_info() first!!!");
+    ok = false;
+  }
+  if(!stop_thread)
+  {
+    ROS_ERROR_STREAM("grasp_storage::save_recording : thread is running, wait for completion!!!");
+    ok = false;
+  }
+  if(obj0_trajectory.empty())
+  {
+    ROS_ERROR_STREAM("grasp_storage::save_recording : trajectory to save is empty - returning!!!");
+    ok = false;
+  }
+  if(!ok)
+  {
+    reset_grasp_storage();
+    return false;
+  }
+  
+  int new_grasp_id = save_in_db(object_id,end_effector_id,grasp_name);
+  if(new_grasp_id <= 0)
+  {
+    reset_grasp_storage();
+    return false;
+  }
+  
+  bool ser_ok = serialize_data((uint)new_grasp_id);
+  
+  if(!ser_ok)
+  {
+    ROS_WARN_STREAM("grasp_storage::save_recording : unable to serialize grasp file - deleting database entry just created (grasp #" << new_grasp_id << ")");
+    bool del_ok = db_writer_->deleteGrasp((uint)new_grasp_id);
+    if(!del_ok)
+      ROS_ERROR_STREAM("grasp_storage::save_recording : unable to delete grasp entry (grasp #" << new_grasp_id << ") from DB - consider deleting it by hand!");
+  }
+  
+  reset_grasp_storage();
+  return ser_ok;
+}
 
-    if (sqlite3_open((path_to_db+"/test.db").c_str(), &db) == SQLITE_OK)
-    {
-	sqlite3_prepare( db, sqlstatement.c_str(), -1, &stmt, NULL );//preparing the statement
-
-	if(sqlite3_step(stmt) == SQLITE_DONE)
-	{
-            scoreID = sqlite3_last_insert_rowid(db);
-            ROS_INFO_STREAM("Last row score : "<<scoreID);
-        }
-        else 
-	{
-	    ROS_ERROR("Failed to store score in cache wihth error message: %s",sqlite3_errmsg(db));
-        }
-    }
-    else
-    {
-        ROS_ERROR("Failed to open db");
-    }
-
-    sqlite3_finalize(stmt);
-    sqlite3_close(db);
+void grasp_storage::stop_recording()
+{
+  stop_record_trajectory_pose();
+  reset_grasp_storage();
 }
 
 bool grasp_storage::save_start_pose()
 {
-    ROS_INFO_STREAM("saving start object position...");
-    bool exit=false;
-    while(!exit)
-    {
-      if (get_transform(world_T_obj0,world_tf,object_tf))
-      {
-	ROS_INFO_STREAM("saved!");
-	return true;
-      }
-      else
-      {
-	ROS_ERROR("could not start saving poses");
-	return false;
-      }
-    }
+  ROS_INFO_STREAM("saving start object position...");
+  if (get_transform(world_T_obj0,world_tf,object_tf,100.0))
+  {
+    ROS_INFO_STREAM("saved!");
+    return true;
+  }
+  else
+  {
+    ROS_ERROR("could not start saving poses");
+    return false;
+  }
 }
 
 void grasp_storage::single_step(bool force_snapshot)
 {
-    geometry_msgs::Pose obj0_traj;
-    tf::StampedTransform world_T_traj;
-    if(save_start)
+  geometry_msgs::Pose obj0_traj;
+  tf::StampedTransform world_T_traj;
+  if(save_start)
+  {
+    if(!save_start_pose())
+      abort();
+    save_start = false;
+    return;
+  }
+  if (get_transform(world_T_traj,world_tf,hand_tf))
+  {
+    // std::cout<<world_T_traj.stamp_<<std::endl;
+    tf::poseTFToMsg(world_T_obj0.inverse()*world_T_traj,obj0_traj);
+    if (force_snapshot)
     {
-      save_start_pose();
-      save_start = false;
+      obj0_trajectory.push_back(obj0_traj);
       return;
     }
-    if (get_transform(world_T_traj,world_tf,hand_tf))
+    for(auto& snapshot_time:snapshots)
     {
-      std::cout<<world_T_traj.stamp_<<std::endl;
-      tf::poseTFToMsg(world_T_obj0.inverse()*world_T_traj,obj0_traj);
-      if (force_snapshot)
+      double diff = fabs((world_T_traj.stamp_ - snapshot_time).toSec());
+      if (diff < 0.03)
       {
-	  obj0_trajectory.push_back(obj0_traj);
-// 	  save_end_pose();
-	  return;
+	obj0_trajectory.push_back(obj0_traj);
+	snapshot_time = ros::Time(ABSURD_TIME);
+	std::cout<<"SAVED snapshot"<<std::endl;
+	save_end_pose();
       }
-      for(auto& snapshot_time:snapshots)
-      {
-	if (fabs((world_T_traj.stamp_-snapshot_time).toSec())<0.03)
-	{
-	  obj0_trajectory.push_back(obj0_traj);
-	  snapshot_time=ros::Time(100);
-	  std::cout<<"SAVED snapshot"<<std::endl;
-	  save_end_pose();
-	}
-      }
+    }
+    
+    // check exit condition : finished last snapshot!
+    if(snapshots.back() == ros::Time(ABSURD_TIME))
+    {
+      stop_record_trajectory_pose();
+    }
   }
 }
 
 void grasp_storage::thread_body()
 {
-    int counter=0;
-    while(!stop_thread)
-    {
-	single_step(false);
-	usleep(10000);
-	counter++;
-    }
-    ROS_INFO_STREAM("stop recording trajectory");
-}
-
-void grasp_storage::record_trajectory_pose()
-{
-    ROS_INFO_STREAM("start recording trajectory");
-
-    tf_periodic_listener = new std::thread(&grasp_storage::thread_body, this);
+  while(!stop_thread)
+  {
+    single_step(false);
+    usleep(THREAD_TIME);
+  }
+  ROS_INFO_STREAM("stop recording trajectory");
 }
 
 void grasp_storage::stop_record_trajectory_pose()
 {
-    stop_thread=true;
+  stop_thread = true;
+  //usleep(100000);
+  //delete tf_periodic_listener;
 }
 
 void grasp_storage::save_end_pose()
 {
-    ROS_INFO_STREAM("saving end position...");
-
-    tf::StampedTransform world_T_hand, world_T_obj;
-    get_transform(world_T_hand,world_tf,hand_tf);
-    
-    get_transform(world_T_obj,world_tf,object_tf);
-    tf::poseTFToMsg(world_T_hand.inverse()*world_T_obj,hand_objF);
-
-//     tf::poseTFToMsg(hand_T_objF,hand_objF);
-
-    ROS_INFO_STREAM("saved!");
+  ROS_INFO_STREAM("saving end position...");
+  
+  tf::StampedTransform world_T_hand, world_T_obj;
+  get_transform(world_T_hand,world_tf,hand_tf);
+  
+  get_transform(world_T_obj,world_tf,object_tf);
+  tf::poseTFToMsg(world_T_hand.inverse()*world_T_obj,hand_objF);
+  
+  ROS_INFO_STREAM("saved!");
 }
 
-void grasp_storage::serialize_data()
+bool grasp_storage::serialize_data(uint new_grasp_id)
 {
-    ROS_INFO_STREAM("serializing...");
-
-    if(scoreID<0)
-    {
-	ROS_ERROR("Row for insertion not valid - ABORT serialization");
-	return;
-    }
-    
-    dual_manipulation_shared::ik_service srv;
-    
-    srv.request.command = "grasp";
-    
-    // create an object for grasping
-    moveit_msgs::AttachedCollisionObject& attached_object = srv.request.attObject;
-    trajectory_msgs::JointTrajectory& grasp_trajectory = srv.request.grasp_trajectory;
-    trajectory_msgs::JointTrajectoryPoint traj_point;
-    
-    // hand: only open to closed
-    traj_point.positions.push_back(0.0);
-    grasp_trajectory.points.push_back(traj_point);
-    traj_point.positions.clear();
-    traj_point.positions.push_back(1.0);
-    grasp_trajectory.points.push_back(traj_point);
-    
-    attached_object.object.id = object_name_map.at(object_id);
-    // this will be interpreted as the object ID (to read in the DB)
-    srv.request.object_db_id = 1;
-
-    // grasp counter!
-    int counter = 0;
-    
-    std::string ee = ee_name_map.at(end_effector_id);
-    
-    if(ee == "left_hand" || ee == "right_hand")
-    {
-      attached_object.link_name = ee + "_palm_link";
-      grasp_trajectory.joint_names.clear();
-      grasp_trajectory.joint_names.push_back(ee + "_synergy_joint");
-    }
-    else
-    {
-      attached_object.link_name = "world";
-      grasp_trajectory.joint_names.clear();
-    }
-      
-    // the frame where the object position is considered (only when inserted)
-    attached_object.object.header.frame_id = attached_object.link_name; //"world";
-    attached_object.object.mesh_poses.clear();
-    attached_object.object.mesh_poses.push_back(hand_objF);
-
-    srv.request.ee_pose.clear();
-    
-    obj0_trajectory.pop_back();
-    for (auto item:obj0_trajectory) srv.request.ee_pose.push_back(item);
-    
-    // save the obtained grasp
-    if(serialize_ik(srv.request,"object" + std::to_string(object_id) + "/grasp" + std::to_string(scoreID)))
-    {
-	ROS_INFO_STREAM("Serialization object" + std::to_string(object_id) << "/grasp" << scoreID << " OK!");
-    }
-    else
-    {
-        std::string temp = std::to_string(object_id);
-	ROS_ERROR("In serialization object%s/grasp%d ! ", temp.c_str(), scoreID);
-	return;
-    }
-    
-    ROS_INFO_STREAM("Serialized");
+  ROS_INFO_STREAM("serializing...");
+  
+  dual_manipulation_shared::grasp_trajectory grasp_msg;
+  
+  // create an object for grasping
+  moveit_msgs::AttachedCollisionObject& attached_object = grasp_msg.attObject;
+  trajectory_msgs::JointTrajectory& grasp_trajectory = grasp_msg.grasp_trajectory;
+  trajectory_msgs::JointTrajectoryPoint traj_point;
+  
+  ROS_WARN_STREAM("grasp_storage::serialize_data : only considering hand synergy from 0.0 to 1.0 - change when more data will be available!!!");
+  // hand: only open to closed
+  traj_point.positions.push_back(0.0);
+  grasp_trajectory.points.push_back(traj_point);
+  traj_point.positions.clear();
+  traj_point.positions.push_back(1.0);
+  grasp_trajectory.points.push_back(traj_point);
+  
+  attached_object.object.id = std::get<0>(db_mapper_->Objects.at(object_id));
+  
+  grasp_msg.object_db_id = 1;
+  
+  // grasp counter!
+  int counter = 0;
+  
+  std::string ee = std::get<0>(db_mapper_->EndEffectors.at(end_effector_id));
+  
+  if(ee == "left_hand" || ee == "right_hand")
+  {
+    attached_object.link_name = ee + "_palm_link";
+    grasp_trajectory.joint_names.clear();
+    grasp_trajectory.joint_names.push_back(ee + "_synergy_joint");
+  }
+  else if(ee == "table")
+  {
+    attached_object.link_name = "world";
+    grasp_trajectory.joint_names.clear();
+  }
+  else
+  {
+    ROS_FATAL_STREAM("grasp_storage::serialize_data : only considering left_hand, right_hand, and table for now - check the code for correctness if more end-effectors are added!!!");
+    abort();
+  }
+  
+  // the frame where the object position is considered (only when inserted)
+  attached_object.object.header.frame_id = attached_object.link_name;
+  attached_object.object.mesh_poses.clear();
+  attached_object.object.mesh_poses.push_back(hand_objF);
+  
+  grasp_msg.ee_pose.clear();
+  
+  obj0_trajectory.pop_back();
+  for (auto item:obj0_trajectory)
+    grasp_msg.ee_pose.push_back(item);
+  
+  // save the obtained grasp
+  bool ok = serialize_ik(grasp_msg,"object" + std::to_string(object_id) + "/grasp" + std::to_string(new_grasp_id));
+  
+  if(ok)
+  {
+    ROS_INFO_STREAM("Serialization object" + std::to_string(object_id) << "/grasp" << new_grasp_id << " OK!");
+  }
+  else
+  {
+    std::string temp = std::to_string(object_id);
+    ROS_ERROR("In serialization object%s/grasp%d ! ", temp.c_str(), new_grasp_id);
+  }
+  
+  return ok;
 }
 
-void grasp_storage::save_in_db()
+int grasp_storage::save_in_db(int obj_id, int ee_id, std::string name)
 {
-    ROS_INFO_STREAM("saving in db...");
-
-    insert_db_entry();
+  ROS_INFO_STREAM("saving in db...");
+  int scoreID = db_writer_->writeNewGrasp(obj_id, ee_id, name);
+  if(scoreID <= 0)
+  {
+    ROS_ERROR_STREAM("grasp_storage::save_in_db : unable to save new grasp " << grasp_name << " for object id " << obj_id << " and end-effector id " << ee_id);
+  }
+  
+  return scoreID;
 }
 
 grasp_storage::~grasp_storage()
 {
-    delete tf_periodic_listener;
+  //delete tf_periodic_listener;
 }
--- grasp_storage/main.cpp
+++ grasp_storage/main.cpp
@@ -1,161 +1,125 @@
 #include "grasp_storage.h"
 #include "ros/ros.h"
+#include <fstream>
 
-#include "dual_manipulation_shared/ik_service.h"
-#include "dual_manipulation_shared/serialization_utils.h"
-#include <kdl/frames.hpp>
-#include <tf_conversions/tf_kdl.h>
+#define DB_NAME "containerB.db"
 
-void thread_body()
+bool read_from_file(std::string filename, std::vector<ros::Time>& v)
 {
-    while(1)
-    {
-	ros::spinOnce(); 
-	usleep(20000);
-    }
-}
-
-int main()
-{
-  dual_manipulation_shared::ik_serviceRequest req;
+  std::fstream fs;
+  // fs.open (path, std::fstream::in | std::fstream::out | std::fstream::app*/);
+  fs.open (filename, std::fstream::in);
+  if (fs.fail())
+      return false;
   
-  deserialize_ik(req,"object3/grasp37");
-  
-  geometry_msgs::Pose obj_pose;
-  KDL::Frame obj_frame;
-  
-//   // invert attached object frame
-//   obj_pose = req.ee_pose.back();
-//   req.ee_pose.pop_back();
-//   
-//   tf::poseMsgToKDL(obj_pose,obj_frame);
-//   tf::poseKDLToMsg(obj_frame.Inverse(),obj_pose);
-//   
-//   req.attObject.object.mesh_poses.clear();
-//   req.attObject.object.mesh_poses.push_back(obj_pose);
-  
-  // add a pre-grasp pose
-//   obj_pose = req.ee_pose.front();
-//   obj_pose.position.x = obj_pose.position.x * 1.5;
-//   obj_pose.position.y = obj_pose.position.y * 1.5;
-//   obj_pose.position.z = obj_pose.position.z * 1.5;
-//   req.ee_pose.insert(req.ee_pose.begin(),obj_pose);
-  
-//   serialize_ik(req,"object3/grasp37");
-  
-//   deserialize_ik(req,"object3/grasp38_old");
-//   // add a pre-grasp pose
-//   obj_pose = req.ee_pose.front();
-//   obj_pose.position.x = obj_pose.position.x * 1.5;
-//   obj_pose.position.y = obj_pose.position.y * 1.5;
-//   obj_pose.position.z = obj_pose.position.z * 1.5;
-//   req.ee_pose.insert(req.ee_pose.begin(),obj_pose);
-
-  KDL::Frame rotx(KDL::Rotation::RotX(M_PI));
-  KDL::Frame obj_hand;
-  
-  for(auto& pose:req.ee_pose)
+  while(1)
   {
-    tf::poseMsgToKDL(pose,obj_hand);
-    tf::poseKDLToMsg(rotx*obj_hand,pose);
+    double tmp;
+    fs >> tmp;
+    if(fs.eof())
+      break;
+    v.push_back(ros::Time(tmp));
   }
   
-  tf::poseMsgToKDL(req.attObject.object.mesh_poses.at(0),obj_frame);
-  obj_hand = obj_frame.Inverse();
-  obj_frame = (rotx*obj_hand).Inverse();
-  tf::poseKDLToMsg(obj_frame,req.attObject.object.mesh_poses.at(0));
-  
-  serialize_ik(req,"object3/grasp38");
-  
-  return 0;
+  fs.close();
+  return true;
 }
 
-int main2(int argc, char** argv)
+int main(int argc, char** argv)
 {
-    if(argc!=4)
-    {
-	std::cout<<std::endl;
-	std::cout<<" - Usage:   ./grasp_storage    object_id    end_effector_id    grasp_name"<<std::endl<<std::endl;
-	std::cout<<"\t - object_id: identifies the object type (1=FakeCylinder, 2=Pot, 3=Cylinder) "<<std::endl;
-	std::cout<<"\t - end_effector_id: identifies the e-e (1=LH, 2=RH, 3=Table)"<<std::endl;
-	std::cout<<"\t - grasp_name: custom name for grasp (e.g. \"RH_top\")"<<std::endl<<std::endl;
-      
-	return -1;
-    }
-
-    int object_id, end_effector_id;
-    std::string grasp_name;
-    
-    object_id = atoi(argv[1]);
-    end_effector_id = atoi(argv[2]);
-    grasp_name = argv[3];
-    
+  if(argc < 5)
+  {
     std::cout<<std::endl;
-    std::cout<<" - Ready to store grasp for:"<<std::endl<<std::endl;
-    std::cout<<"\t - object_id = "<<object_id<<std::endl;
-    std::cout<<"\t - end_effector_id = "<<end_effector_id<<std::endl;
-    std::cout<<"\t - grasp_name = "<<grasp_name<<std::endl<<std::endl;
-    std::cout<<"No data will be saved on disk until the \"!! ATTENTION !!\" messages"<<std::endl<<std::endl;
+    std::cout<<" - Usage[1]:   ./grasp_storage    object_id    end_effector_id    grasp_name    snapshot_time0    snapshot_time1 ..."<<std::endl<<std::endl;
+    std::cout<<"\t - object_id: identifies the object type"<<std::endl;
+    std::cout<<"\t - end_effector_id: identifies the e-e"<<std::endl;
+    std::cout<<"\t - grasp_name: custom name for grasp (e.g. \"RH_top\")"<<std::endl;
+    std::cout<<"\t - snapshot_times (at least 2): timing vector to use for taking snapshots"<<std::endl<<std::endl;
+    std::cout<<" - Usage[2]:   ./grasp_storage    object_id    end_effector_id    grasp_name    filename"<<std::endl<<std::endl;
+    std::cout<<"\t - filename: file from which snapshot times will be read"<<std::endl<<std::endl;
   
-    ros::init(argc, argv, "grasp_storage");
-    
-    std::thread th(&thread_body);
-    
-    grasp_storage grasp_stor(object_id, end_effector_id, grasp_name);
-    
-    char a;
+    return -1;
+  }
 
-    ROS_INFO("Press any key ('q' to exit, 's' to snapshot) to START recording TRAJECTORY: ");
-//     ROS_INFO("Press any key ('q' to exit) to save the START POSITION: ");
-    std::cin>>a;
-    if(a=='q') return 0;
-    if (a=='s')
+  int object_id, end_effector_id;
+  std::string grasp_name;
+  std::vector<ros::Time> snapshots;
+  
+  object_id = atoi(argv[1]);
+  end_effector_id = atoi(argv[2]);
+  grasp_name = argv[3];
+  
+  if(argc == 5)
+  {
+    // std::string filename = ros::package::getPath("dual_manipulation_ik_control");
+    // filename.append(argv[4]);
+    std::string filename = argv[4];
+    read_from_file(filename,snapshots);
+  }
+  else
+  {
+    double snap_time;
+    int counter = 4;
+    while(counter < argc)
     {
-      bool end=false;
-      while (!end)
-      {
-	ROS_INFO("Press 's' key ('f' for final pose, 'q' to exit) to snapshot: ");
-	std::cin>>a;
-	if(a=='q') end=true;
-	if(a=='s') grasp_stor.single_step(true);
-	if(a=='f') grasp_stor.save_end_pose();
-      }
+      snap_time = atoi(argv[counter++]);
+      snapshots.push_back(ros::Time(snap_time));
     }
-    else
-    {
-    grasp_stor.save_start_pose();
-    
-//     ROS_INFO("Press any key ('q' to exit) to START recording TRAJECTORY: ");
-//     std::cin>>a;
-//     if(a=='q') return 0;
-    grasp_stor.record_trajectory_pose();
-    
-    ROS_INFO("Press any key ('q' to exit) to STOP recording TRAJECTORY: ");
-    std::cin>>a;
-    if(a=='q') return 0;
-    grasp_stor.stop_record_trajectory_pose();
-    }
-//     usleep(200000);
-    
-//     ROS_INFO("Press any key ('q' to exit) to save the END POSITION: ");
-//     std::cin>>a;
-//     if(a=='q') return 0;
-//     grasp_stor.save_end_pose();
-    
-    ROS_INFO("!! ATTENTION !! Press any key ('q' to exit) to insert row in the database ('n' to abort) : ");
-    std::cin>>a;
-    if(a=='q') return 0;
-    if(a!='n') grasp_stor.save_in_db();
-    else ROS_WARN("ABORT writing in database");
-    
-    ROS_INFO("!! ATTENTION !! Press any key ('q' to exit) to serialize data ('n' to abort) : ");
-    std::cin>>a;
-    if(a=='q') return 0;
-    if(a!='n') grasp_stor.serialize_data();
-    else ROS_WARN("ABORT serialization");
-    
-    std::cout<<std::endl<<">>>>> Press any key to close <<<<<<"<<std::endl<<std::endl;
-    std::cin>>a;
-    
+  }
+  
+  std::cout << "Snapshots: ";
+  for(auto s:snapshots)
+    std::cout << s.toSec() << " ";
+  std::cout << std::endl;
+  
+  std::cout<<std::endl;
+  std::cout<<" - Ready to store grasp for:"<<std::endl<<std::endl;
+  std::cout<<"\t - object_id = "<<object_id<<std::endl;
+  std::cout<<"\t - end_effector_id = "<<end_effector_id<<std::endl;
+  std::cout<<"\t - grasp_name = "<<grasp_name<<std::endl;
+  std::cout<<"\t - "<<snapshots.size()<<" snapshots in total"<<std::endl<<std::endl;
+  std::cout<<"No data will be saved on disk until the \"!! ATTENTION !!\" messages"<<std::endl<<std::endl;
+
+  ros::init(argc, argv, "grasp_storage");
+  
+  grasp_storage graspStorage(DB_NAME);
+  
+  if(!graspStorage.set_grasp_info(object_id, end_effector_id, grasp_name,snapshots))
+  {
+    ROS_ERROR_STREAM("Unable to set required information in graspStorage - returning...");
+    return -1;
+  }
+  
+  char a;
+
+  ROS_INFO("Press 'q' to exit, or any other key to START recording TRAJECTORY: ");
+  
+  std::cin>>a;
+  if(a=='q')
     return 0;
+  
+  if(!graspStorage.record_grasp())
+  {
+    ROS_ERROR_STREAM("Unable to record required grasp - returning...");
+    return -1;
+  }
+  
+  while(!graspStorage.finished())
+    usleep(100000);
+
+  ROS_INFO("!! ATTENTION !! Recording ended, press 'q' to exit without saving or any other key to insert row in the database and generate grasp file: ");
+
+  std::cin>>a;
+  if(a=='q')
+    return 0;
+
+  if(!graspStorage.save_recording())
+  {
+    ROS_ERROR_STREAM("Unable to saved recorded information in the DB/file - returning...");
+    return -1;
+  }
+  
+  ROS_INFO("Recording done!");
+  
+  return 0;
 }
