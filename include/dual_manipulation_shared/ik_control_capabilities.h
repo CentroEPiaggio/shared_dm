014c4938fc186fcb0e19d7afff1358b15006b70c
hamalMarino <hamal.marino@gmail.com>
2015-04-28 15:36:14 +0200

    considered the possibility of planning best effort without collision checking

--- include/dual_manipulation_shared/ik_control_capabilities.h
+++ include/dual_manipulation_shared/ik_control_capabilities.h
@@ -9,6 +9,7 @@
 #define PLAN_CAPABILITY "plan"
 #define PLAN_NO_COLLISION_CAPABILITY "plan_no_collision"
 #define PLAN_BEST_EFFORT_CAPABILITY "plan_best_effort"
+#define PLAN_BEST_EFFORT_NO_COLLISION_CAPABILITY "plan_best_effort_no_collision"
 #define MOVE_CAPABILITY "execute"
 #define GRASP_CAPABILITY "grasp"
 #define UNGRASP_CAPABILITY "ungrasp"
@@ -19,6 +20,7 @@
 #define PLAN_MSG "planning_done"
 #define PLAN_NO_COLLISION_MSG "planning_done"
 #define PLAN_BEST_EFFORT_MSG "planning_done"
+#define PLAN_BEST_EFFORT_NO_COLLISION_MSG "planning_done"
 #define MOVE_MSG "action_done"
 #define GRASP_MSG "grasp_done"
 #define UNGRASP_MSG "grasp_done"
@@ -41,6 +43,7 @@ enum class ik_control_capabilities
   PLAN,
   PLAN_NO_COLLISION,
   PLAN_BEST_EFFORT,
+  PLAN_BEST_EFFORT_NO_COLLISION,
   MOVE,
   GRASP,
   UNGRASP,
@@ -57,6 +60,7 @@ public:
     name[ik_control_capabilities::PLAN] = PLAN_CAPABILITY;
     name[ik_control_capabilities::PLAN_NO_COLLISION] = PLAN_NO_COLLISION_CAPABILITY;
     name[ik_control_capabilities::PLAN_BEST_EFFORT] = PLAN_BEST_EFFORT_CAPABILITY;
+    name[ik_control_capabilities::PLAN_BEST_EFFORT_NO_COLLISION] = PLAN_BEST_EFFORT_NO_COLLISION_CAPABILITY;
     name[ik_control_capabilities::MOVE] = MOVE_CAPABILITY;
     name[ik_control_capabilities::GRASP] = GRASP_CAPABILITY;
     name[ik_control_capabilities::UNGRASP] = UNGRASP_CAPABILITY;
@@ -67,6 +71,7 @@ public:
     from_name[PLAN_CAPABILITY] = ik_control_capabilities::PLAN;
     from_name[PLAN_NO_COLLISION_CAPABILITY] = ik_control_capabilities::PLAN_NO_COLLISION;
     from_name[PLAN_BEST_EFFORT_CAPABILITY] = ik_control_capabilities::PLAN_BEST_EFFORT;
+    from_name[PLAN_BEST_EFFORT_NO_COLLISION_CAPABILITY] = ik_control_capabilities::PLAN_BEST_EFFORT_NO_COLLISION;
     from_name[MOVE_CAPABILITY] = ik_control_capabilities::MOVE;
     from_name[GRASP_CAPABILITY] = ik_control_capabilities::GRASP;
     from_name[UNGRASP_CAPABILITY] = ik_control_capabilities::UNGRASP;
@@ -77,6 +82,7 @@ public:
     msg[ik_control_capabilities::PLAN] = PLAN_MSG;
     msg[ik_control_capabilities::PLAN_NO_COLLISION] = PLAN_NO_COLLISION_MSG;
     msg[ik_control_capabilities::PLAN_BEST_EFFORT] = PLAN_BEST_EFFORT_MSG;
+    msg[ik_control_capabilities::PLAN_BEST_EFFORT_NO_COLLISION] = PLAN_BEST_EFFORT_NO_COLLISION_MSG;
     msg[ik_control_capabilities::MOVE] = MOVE_MSG;
     msg[ik_control_capabilities::GRASP] = GRASP_MSG;
     msg[ik_control_capabilities::UNGRASP] = UNGRASP_MSG;
@@ -87,6 +93,7 @@ public:
     type[ik_control_capabilities::PLAN] = ik_control_capability_types::PLAN;
     type[ik_control_capabilities::PLAN_NO_COLLISION] = ik_control_capability_types::PLAN;
     type[ik_control_capabilities::PLAN_BEST_EFFORT] = ik_control_capability_types::PLAN;
+    type[ik_control_capabilities::PLAN_BEST_EFFORT_NO_COLLISION] = ik_control_capability_types::PLAN;
     type[ik_control_capabilities::MOVE] = ik_control_capability_types::MOVE;
     type[ik_control_capabilities::GRASP] = ik_control_capability_types::GRASP;
     type[ik_control_capabilities::UNGRASP] = ik_control_capability_types::GRASP;
