26f809a009ae485ee3d614b5546159823f0e4579
hamalMarino <hamal.marino@gmail.com>
2015-05-05 11:35:51 +0200

    added definitions of SET_TARGET and SET_HOME_TARGET capabilities

--- include/dual_manipulation_shared/ik_control_capabilities.h
+++ include/dual_manipulation_shared/ik_control_capabilities.h
@@ -4,7 +4,8 @@
 #include <map>
 #include <string>
 
-// #define SET_TARGET_CAPABILITY "set_target"
+#define SET_TARGET_CAPABILITY "set_target"
+#define SET_HOME_TARGET_CAPABILITY "set_home_target"
 #define IK_CHECK_CAPABILITY "ik_check"
 #define PLAN_CAPABILITY "plan"
 #define PLAN_NO_COLLISION_CAPABILITY "plan_no_collision"
@@ -15,7 +16,8 @@
 #define UNGRASP_CAPABILITY "ungrasp"
 #define HOME_CAPABILITY "home"
 
-// #define SET_TARGET_MSG "set_target_done"
+#define SET_TARGET_MSG "set_target_done"
+#define SET_HOME_TARGET_MSG "set_target_done"
 #define IK_CHECK_MSG "check_done"
 #define PLAN_MSG "planning_done"
 #define PLAN_NO_COLLISION_MSG "planning_done"
@@ -25,11 +27,10 @@
 #define GRASP_MSG "grasp_done"
 #define UNGRASP_MSG "ungrasp_done"
 #define HOME_MSG "action_done"
-// #define HOME_MSG "set_target_done"
 
 enum class ik_control_capability_types
 {
-//   SET_TARGET,
+  SET_TARGET,
   IK_CHECK,
   PLAN,
   MOVE,
@@ -38,7 +39,8 @@ enum class ik_control_capability_types
 
 enum class ik_control_capabilities
 {
-//   SET_TARGET,
+  SET_TARGET,
+  SET_HOME_TARGET,
   IK_CHECK,
   PLAN,
   PLAN_NO_COLLISION,
@@ -55,7 +57,8 @@ class ik_control_capability
 public:
   ik_control_capability()
   {
-    // name[ik_control_capabilities::SET_TARGET] = SET_TARGET_CAPABILITY;
+    name[ik_control_capabilities::SET_TARGET] = SET_TARGET_CAPABILITY;
+    name[ik_control_capabilities::SET_HOME_TARGET] = SET_HOME_TARGET_CAPABILITY;
     name[ik_control_capabilities::IK_CHECK] = IK_CHECK_CAPABILITY;
     name[ik_control_capabilities::PLAN] = PLAN_CAPABILITY;
     name[ik_control_capabilities::PLAN_NO_COLLISION] = PLAN_NO_COLLISION_CAPABILITY;
@@ -66,7 +69,8 @@ public:
     name[ik_control_capabilities::UNGRASP] = UNGRASP_CAPABILITY;
     name[ik_control_capabilities::HOME] = HOME_CAPABILITY;
     
-    // from_name[SET_TARGET_CAPABILITY] = ik_control_capabilities::SET_TARGET;
+    from_name[SET_TARGET_CAPABILITY] = ik_control_capabilities::SET_TARGET;
+    from_name[SET_HOME_TARGET_CAPABILITY] = ik_control_capabilities::SET_HOME_TARGET;
     from_name[IK_CHECK_CAPABILITY] = ik_control_capabilities::IK_CHECK;
     from_name[PLAN_CAPABILITY] = ik_control_capabilities::PLAN;
     from_name[PLAN_NO_COLLISION_CAPABILITY] = ik_control_capabilities::PLAN_NO_COLLISION;
@@ -77,7 +81,8 @@ public:
     from_name[UNGRASP_CAPABILITY] = ik_control_capabilities::UNGRASP;
     from_name[HOME_CAPABILITY] = ik_control_capabilities::HOME;
     
-    // msg[ik_control_capabilities::SET_TARGET] = SET_TARGET_MSG;
+    msg[ik_control_capabilities::SET_TARGET] = SET_TARGET_MSG;
+    msg[ik_control_capabilities::SET_HOME_TARGET] = SET_HOME_TARGET_MSG;
     msg[ik_control_capabilities::IK_CHECK] = IK_CHECK_MSG;
     msg[ik_control_capabilities::PLAN] = PLAN_MSG;
     msg[ik_control_capabilities::PLAN_NO_COLLISION] = PLAN_NO_COLLISION_MSG;
@@ -88,7 +93,8 @@ public:
     msg[ik_control_capabilities::UNGRASP] = UNGRASP_MSG;
     msg[ik_control_capabilities::HOME] = HOME_MSG;
     
-    // type[ik_control_capabilities::SET_TARGET] = ik_control_capability_types::SET_TARGET;
+    type[ik_control_capabilities::SET_TARGET] = ik_control_capability_types::SET_TARGET;
+    type[ik_control_capabilities::SET_HOME_TARGET] = ik_control_capability_types::SET_TARGET;
     type[ik_control_capabilities::IK_CHECK] = ik_control_capability_types::IK_CHECK;
     type[ik_control_capabilities::PLAN] = ik_control_capability_types::PLAN;
     type[ik_control_capabilities::PLAN_NO_COLLISION] = ik_control_capability_types::PLAN;
@@ -98,8 +104,8 @@ public:
     type[ik_control_capabilities::GRASP] = ik_control_capability_types::GRASP;
     type[ik_control_capabilities::UNGRASP] = ik_control_capability_types::GRASP;
     type[ik_control_capabilities::HOME] = ik_control_capability_types::MOVE;
-    // type[ik_control_capabilities::HOME] = ik_control_capability_types::SET_TARGET;
     
+    implemented_for_trees[ik_control_capability_types::SET_TARGET] = true;
     implemented_for_trees[ik_control_capability_types::IK_CHECK] = true;
     implemented_for_trees[ik_control_capability_types::PLAN] = true;
     implemented_for_trees[ik_control_capability_types::MOVE] = true;
