9868ce5c0caf97dbf18d8a653d3f7207819b8723
Hamal Marino <hamal.marino@gmail.com>
2016-09-14 19:28:49 +0200

    moved node_properties to shared, to be used also from databaseMapper

---
+++ include/dual_manipulation_shared/node_transitions.h
@@ -0,0 +1,51 @@
+#ifndef NODE_TRANSITIONS_H
+#define NODE_TRANSITIONS_H
+#include <map>
+
+/**
+ * @brief A class for defining all possible transitions which are allowed between nodes in the graph for planning
+ */
+namespace dual_manipulation
+{
+namespace shared
+{
+
+static const std::string GRASP_NAME("grasp");
+static const std::string UNGRASP_NAME("ungrasp");
+static const std::string EXCHANGE_GRASP_NAME("exchange_grasp");
+
+enum class NodeTransitionTypes
+{
+    UNKNOWN,                    // transition to consider empty or non-existent fields: will be found in the old way, for back-compatibility
+    GRASP,                      // from one non-movable to one movable, grasp the object (change on ground)
+    UNGRASP,                    // from one movable to one non-movable, ungrasp the object (change on ground)
+    EXCHANGE_GRASP,             // both ee are movable, perform integrasp (change above ground)
+    LAST_EE_FIXED,              // ee_id is the last end effector in the path, and is not movable
+    LAST_EE_MOVABLE             // ee_id is the last end effector in the path, and is movable
+};
+
+class NodeTransitions
+{
+public:
+    NodeTransitions()
+    {
+        fromName[GRASP_NAME] = NodeTransitionTypes::GRASP;
+        fromName[UNGRASP_NAME] = NodeTransitionTypes::GRASP;
+        fromName[EXCHANGE_GRASP_NAME] = NodeTransitionTypes::GRASP;
+    }
+    NodeTransitionTypes getTransitionTypeFromName(const std::string& name) const
+    {
+        if(!fromName.count(name))
+            return NodeTransitionTypes::UNKNOWN;
+        
+        return fromName.at(name);
+    }
+    
+private:
+    std::map<std::string,NodeTransitionTypes> fromName;
+};
+
+}
+}
+
+#endif // NODE_TRANSITIONS_H
